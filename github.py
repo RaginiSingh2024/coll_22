4d4116dcb216a7213d533aee326b2f0f499e11b9
rehman0601 <rehman65438@gmail.com>
2025-01-24 15:04:24 +0530

    input added

--- github.py
+++ github.py
@@ -3,6 +3,12 @@ class cylinder:
         self.radius=radius
         self.height=height
     
+
+height = float(input("Enter the height of the cylinder: "))
+radius = float(input("Enter the radius of the cylinder: "))
+a = cylinder(3,4)
+print("The radius of the cylinder is ",a.radius)
+print("height of the cylinder is ",a.height)
         
 
 
